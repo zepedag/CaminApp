d8000f84369fbb2706494ab307ef710aea841afd
Nina Maeva Mazadiego Cortes <mazcortmae@gmail.com>
2025-04-02 02:01:02 -0600

    initial preview of Social tab

---
+++ CaminaApp/Views/Social/SocialGeneralView.swift
@@ -0,0 +1,18 @@
+//
+//  SocialGeneralView.swift
+//  CaminaApp
+//
+//  Created by Maeva Mazadiego on 02/04/25.
+//
+
+import SwiftUI
+
+struct SocialGeneralView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SocialGeneralView()
+}
