ead4cd7c6dffbe96df4043936f7091280e4867f9
3str3ll4V3r <156131289+3str3ll4V3r@users.noreply.github.com>
2025-04-02 01:25:56 -0600

    Colors2.0

--- CaminaApp/Resources/Colors.swift
+++ CaminaApp/Resources/Colors.swift
@@ -3,15 +3,15 @@ import SwiftUI
 import UIKit
 
 extension Color {
-    public static let white = Color(hex: "#F6F6F6")
-    public static let gray = Color(hex: "#E8E8E8")
-    public static let black = Color(hex: "#333333")
-    public static let PrimaryRed = Color(hex: "#990100")
-    public static let SecondRed = Color(hex: "#B90504")
-    public static let navyBlue = Color(hex: "#003049")
-    public static let mustardYellow = Color(hex: "#FFB703")
+    public static let primaryGreen = Color(hex: "#B90504")
+    public static let darkGreen = Color(hex: "#990100")
+    public static let cream = Color(hex: "#F6F6F6")
+    public static let orange = Color(hex: "#E8E8E8")
+    public static let brown = Color(hex: "#333333")
+    public static let navyBlue = Color(hex: "#E8E8E8")
+    public static let mustardYellow = Color(hex: "#B90504")
     public static let opaqueRed = Color(hex: "#C1121F")
-    public static let lightGreen = Color(hex: "#A7C957")
+    public static let lightGreen = Color(hex: "#E8E8E8")
 }
 
 extension Color {
