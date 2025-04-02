0a651c4128dd487f044521cb1b1b5daf3565af0c
Alejandro Zepeda <humberto.zepedagz@udlap.mx>
2025-04-02 11:10:58 -0600

    Search Functionality in Home page and Search view

---
+++ CaminaApp/Views/Restaurant/RestaurantCardComponents.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  CaminaApp
+//
+//  Created by Emi on 02/04/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
